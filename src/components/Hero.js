99196e0ab4667c8806f0c03465a720df7892ebbb
Joyce <joycethhh@gmail.com>
2024-11-14 21:09:11 -0600

    hero basic layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,21 @@ import React from "react";
 
 export default function Hero ({text}) {
     return (
-        <div style={{backgroundImage:"url(`images/hero.jpg`)"}} >
-           <h2>{text}</h2> 
+        <div  
+            style={{
+                backgroundImage: "url('/images/hero.jpg')",
+                backgroundSize: 'cover',
+                backgroundPosition:'center',
+                height: '400px',
+                position: 'relative',
+            }}
+            className="flex items-center justify-center text-white"
+        >
+            <div className="absolute bottom-12 w-full text-center">
+            <h2 className="text-4xl font-bold text-white text-opacity-60">JEWELRY STORE</h2> 
+            <p className="text-xl mt-2 text-white text-opacity-70"> Where elegance meet timeless beauty</p>
+            </div>
+           
         </div>
     )
 }
