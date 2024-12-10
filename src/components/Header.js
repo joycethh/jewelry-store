53d023fb10cadf5fe478be8f1505e6bc9752493e
Joyce <joycethhh@gmail.com>
2024-12-10 16:10:48 -0600

    fixed navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,39 +25,39 @@ export default function Header () {
   return (
     <>
       <header className="fixed top-0 left-0 w-full z-50" >
-      {/* real header */}
+      {/* header with brand */}
       <div className={`w-full h-16 flex items-center justify-between px-6 transistion-all duration-200 ${isScrolled ? 'bg-white text-textDark' : 'bg-transparent text-white'}`}>
-      <h1 className='uppercase tracking-widest text-3xl md:text-2xl font-bold mx-auto'>jewelry store</h1>
+      <h1 className='uppercase tracking-[0.2em] text-3xl md:text-2xl font-bold mx-auto'>Jewelry store</h1>
         <div className='flex space-x-4'>
         <UserIcon className='h-6 w-6 text-custom-dark' />
         <ShoppingBagIcon className='h-6 w-6 text-custom-dark'/>
         </div>
       </div>
 
+
       {/* navbar */}
-        <div className={`w-full transition-all duration-200 ${isScrolled ? 'bg-white text-textDark shadow-md' : 'bg-transparent text-white'}`}>
-        <nav className="border-2 bg-transparent w-full shadow transition-all duration-200">
-     
-     {/* menu */}
-      <ul className="flex justify-center space-x-8 py-4 text-white uppercase tracking-wide">
-        {categoriesData.categories.map((category) => {
-        //   console.log("category", category);
+      <div className={`w-full transition-all duration-200 ${isScrolled ? 'bg-white text-textDark' : 'bg-transparent text-white'}`}> 
+      {/* menu */}
+      <nav className="w-full px-5 pb-3">
+        <ul className="flex space-x-10 uppercase tracking-widest">
+          {categoriesData.categories.map((category) => {
+          //console.log("category", category);
           return (
             <li 
                 key={category.name} 
-                className="relative"
+                className="relative group"
                 onMouseEnter={() =>handleMouseEnter(category.name)}
                 onMouseLeave={handleMouseLeave}
                 >
-              <span>{category.name}</span>
+              <span className="cursor-pointer hover:underline">{category.name}</span>
               
               
-              {/* submenu */}
-              <ul className="absolute group top-full left-0 bg-white text-textDark shadow-lg py-2">
+              {/* drowpdown submenu */}
+              <ul className="absolute top-full left-0 bg-white text-textDark shadow-lg py-4">
                 {dropdown === category.name && category.subcategories.map((subcategory) => {
                 //   console.log("subcategory", subcategory);
                   return (
-                    <li key={subcategory} className="px-4 py-2 hover: underline cursor-pointer">
+                    <li key={subcategory} className="px-6 py-2 hover:bg-gray-100 hover:underline cursor-pointer">
                       <Link to={`/products/jewelry-by-${category.name.toLowerCase()}/${subcategory
                         .toLowerCase()
                         .replace(' ', '-')}`}>{subcategory}</Link>
